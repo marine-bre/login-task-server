d7612d208117b68d4c221a4cd8c0057c10d6779a
marine-bre <marine_bretonniere@hotmail.co.uk>
2020-12-19 13:39:12 +0200

    fixed

--- index.js
+++ index.js
@@ -2,11 +2,23 @@ const express = require('express');
 const app = express();
 const userRoutes = require('./routes/users.js')
 const taskRoutes = require('./routes/tasks.js')
+
 const dotenv = require('dotenv').config()
 const mongoose = require('mongoose')
 const bp = require('body-parser')
 
 const URI = process.env.MONGODB_URI;
+
+// app.get('/', (req, res) => {
+//     res.send('hia')
+// })
+
+let allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Headers', "*");
+    next();
+  }
+  app.use(allowCrossDomain);
 
 app.use(bp.json())
 app.use(bp.urlencoded({ extended: true }))
